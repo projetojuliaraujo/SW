e2f850282f2f848964be875a606e80886018fd84
Júlia Araújo Nogueira de Freitas <100208717+projetojuliaraujo@users.noreply.github.com>
2023-03-28 12:30:29 -0300

    Add files via upload

---
+++ Projeto_Loja/Produto.cs
@@ -0,0 +1,31 @@
+﻿namespace Projeto_Loja;
+class Produto
+{
+    public Produto(){
+        this.Nome = "TICTOC";
+        this.Preco = 15;
+    }
+
+    public Produto(string nome){
+        this.Nome = nome;
+        this.Preco = 10;
+    }
+
+    public Produto(string nome, int desconto){
+        this.Nome = nome;
+        this.Preco = 10 - (10*desconto/100);
+    }
+    private string? Nome {get;set;}
+    private double Preco {get;set;}
+
+    public void AlterarNome(string nome){
+        this.Nome = nome;
+    }
+    public void AlterarPreco(double preco){
+        this.Preco = preco;
+    }
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + this.Nome);
+        Console.WriteLine("Preco: " + this.Preco);
+    }
+}
