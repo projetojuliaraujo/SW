575763951701d981e4a8a9b5619096ef99d3074d
Júlia Araújo Nogueira de Freitas <100208717+projetojuliaraujo@users.noreply.github.com>
2023-03-28 12:31:15 -0300

    Add files via upload

---
+++ PROJ_PESSOA/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿namespace PROJ_PESSOA;
+class Pessoa
+{
+    public Pessoa(){
+        this.Nome = "NÃO INFORMADO";
+        this.SobreNome = "NÃO INFORMADO";
+        this.Idade = 0;
+    }
+     public Pessoa(string nome, int idade){
+        this.Nome = nome;
+        this.SobreNome = "NÃO INFORMADO";
+        this.Idade = idade;
+    }
+    private string? Nome {get;set;}
+
+    private string? SobreNome {get;set;}
+
+    private int Idade {get; set;}
+
+    public void AlterarNome(string nome){
+      this.Nome = nome;
+    }
+    public void AlterarSobreNome(string sobrenome){
+      this.SobreNome = sobrenome;
+    }
+    public void AlterarIdade(int idade){
+      this.Idade = idade;
+    }
+
+    public void MostraDados(){
+       Console.WriteLine("Nome: " + this.Nome);
+       Console.WriteLine("SobreNome: " + this.SobreNome);
+       Console.WriteLine("Idade: " + this.Idade);
+    } 
+}
+
---
+++ PROJ_PESSOA/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PROJ_PESSOA;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("GODOFREDO", 18);
+        //p1.AlterarNome("Júlia");
+        //p1.AlterarSobreNome("Araújo");
+        //p1.AlterarIdade(16);
+        p1.MostraDados();
+    }
+}
