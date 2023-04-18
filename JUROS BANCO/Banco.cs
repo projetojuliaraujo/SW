27f65a68c1d7c14d2a7dc4c2cb3ec82af5481bc7
Júlia Araújo Nogueira de Freitas <100208717+projetojuliaraujo@users.noreply.github.com>
2023-04-18 12:32:19 -0300

    Add files via upload

---
+++ JUROS BANCO/Banco.cs
@@ -0,0 +1,24 @@
+﻿namespace JUROS_BANCO;
+class Banco
+{ 
+ public int numero1 {get;set;}  
+ public int numero2 {get;set;}  
+
+  public string dividir (int num1, int num2){
+    this.numero1 = num1;
+    this.numero2 = num2;
+    int dividir = this.numero1/this.numero2;
+    string resul1 = "A quantia emprestada é de R$" +this.numero1+ " com o prazo de " + this.numero2+ " meses é de: R$" + dividir + " sem juros"; 
+
+    return resul1;
+}
+
+    public double MostrarSaldo(){
+        return this.numero1 + this.numero2;
+    }
+
+    public void AjustaLimite(double valor){
+        this.numero1 = valor;
+    }
+}
+
