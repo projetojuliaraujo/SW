cb734393a18b9367bea889029ffb7e2630a8f28d
Júlia Araújo Nogueira de Freitas <100208717+projetojuliaraujo@users.noreply.github.com>
2023-04-25 12:04:40 -0300

    Add files via upload

---
+++ JUROS BANCO/JUROS BANCO/Banco.cs
@@ -0,0 +1,44 @@
+﻿namespace   JUROS_BANCO;
+class Banco
+{   
+    public int Numero {get; set;}
+    private double Saldo { get;  set;}
+    public double Limite {get; private set;}
+
+    public void AjustarLimite(double valor){
+        this.Limite = valor;
+    }
+    public void Depositar( double valor){
+        this.Saldo += valor;
+    }
+    public void Sacar(double valor){
+        if (this.Saldo + this.Limite <= valor){
+            Console.WriteLine("Valor Inválido! Você está tentando sacar mais do que você tem!");
+        }else{
+             this.Saldo -= valor;
+        }
+    }
+    public double MostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    public double parcela {get; set;}
+    public double valor {get; set;}
+
+    public string emprestimo(int parcelas, double dinheiro){
+        this.parcela = parcelas;
+        this.valor = dinheiro;
+        
+        double div = dinheiro/parcelas;
+        double mult = dinheiro * 0.06;
+        double soma = mult + div;
+        double total = soma *parcelas;
+
+        string resultado = "O valor de sua parcela sem juros é: "+ div +". E o valor da parcela com juros: "+ mult + ". O valor do juros com prestações é de: "+ soma +"e o valor total a pagar é de: "+total;
+        return resultado;
+        
+    }
+    public void imprimi(string texto){
+        Console.WriteLine(texto);
+    }
+}
